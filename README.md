82547e3f6a14c3aaeb7b62d2c32a11fc5bb6119d
qwerty88T <guiongblacksire@gmail.com>
2022-10-03 18:30:14 +0800

    1st

---
+++ README.md
@@ -0,0 +1,5 @@
+# finallydoneit package
+
+A short description of your package.
+
+THis asdasdzlkxnczl xpczmpxc zpxkc pzokcpz okcpzo kxpck z
---
+++ spec/finallydoneit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FinallydoneitView from '../lib/finallydoneit-view';
+
+describe('FinallydoneitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
