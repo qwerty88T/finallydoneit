b31c7547c69b8cdef1ac539e6fb540f2c9b707a1
qwerty88T <guiongblacksire@gmail.com>
2022-10-03 18:39:55 +0800

    z

--- spec/finallydoneit-view-spec.js
+++ spec/finallydoneit-view-spec.js
@@ -1,3 +1,13 @@
 'use babel';
 
 import FinallydoneitView from '../lib/finallydoneit-view';
+
+describe("A suite is just a function", function() {
+  let a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
