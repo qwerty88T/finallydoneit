0b89b05fefb6417c8dd3d6adb7af7193dc281e14
qwerty88T <guiongblacksire@gmail.com>
2022-10-03 18:31:53 +0800

    z

--- spec/finallydoneit-view-spec.js
+++ spec/finallydoneit-view-spec.js
@@ -1,9 +1,3 @@
 'use babel';
 
 import FinallydoneitView from '../lib/finallydoneit-view';
-
-describe('FinallydoneitView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
